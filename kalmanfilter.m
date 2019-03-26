c4cb01bdb625076544cd629a83909495ae46d556
LorenzoDag <s1085177@studenti.univpm.it>
2019-03-26 19:05:05 +0100

    oggi

--- kalmanfilter.m
+++ kalmanfilter.m
@@ -1,22 +1,39 @@
-classdef kalmanfilter < handle
+classdef kalmanfilter < sistema
     %KALMAN Summary of this class goes here
     %   Detailed explanation goes here
     
     properties
-        sigmaModel, P, K, stima;
+        sigmaModel, P, K;
     end
     
     methods
-        function obj = kalmanfilter(sigmaModel)
+        function obj = kalmanfilter(sigmaModel, x0)
             % inizializzo la stima come l'uscita del sistema da osservare
+            obj@sistema(sigmaModel.A, sigmaModel.B, sigmaModel.C, sigmaModel.Q, sigmaModel.R, x0);
             obj.sigmaModel = sigmaModel;
-            obj.P = inv(sigmaModel.C)*sigmaModel.R*inv(sigmaModel.C');
-            obj.stima = sigmaModel.leggiUscita();
+            obj.P = eye(obj.n);
         end
         
         function kalmanGain(obj)
             % calcolo della matrice di guadagno di kalman
-            obj.K = obj.P*obj.sigmaModel.C'*inv(obj.sigmaModel.C*obj.P*obj.sigmaModel.C'+obj.sigmaModel.R);
+            obj.K = obj.P*obj.C'*inv(obj.C*obj.P*obj.C'+obj.R);
+        end
+        
+        function update(obj, u, y)
+            %predizione
+            obj.x = obj.A*obj.x + obj.B*u;
+            obj.P = obj.A*obj.P*obj.A'+obj.Q;
+            
+            obj.kalmanGain();
+            
+            %aggiornamento
+            obj.x = obj.x+obj.K*(y-obj.C*obj.x);
+            obj.P = (eye(obj.n)-obj.K*obj.C)*obj.P;
+            
+        end
+            
+        function y = leggiUscita(obj)
+            y = obj.x;
         end
         
     end
